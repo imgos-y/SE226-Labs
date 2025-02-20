c902a1b4509dbf7174fdcdde4e90d9e636e10827
imgos-y <imgeyuksel427@gmail.com>
2025-02-20 15:27:59 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,61 @@
+// q1
+#include <iostream>
+using namespace std;
+int main() {
+   /* string name;
+    string student_id;
+    
+    cout << "What is your name: "<< endl;
+    cin>> name;
+    cout << "Hello "<< name << " . " << endl;
+    cout << "What is your student id: " << endl;
+    cin >> student_id;
+    cout << "Your id is:  " << student_id << endl;*/
+    
+    // q2
+   /* double var1 , var2;
+    cout<< "Enter your fist number (var1): ";
+    cin>> var1;
+    cout << "Enter your second number (var2): ";
+    cin >> var2;
+    
+    double sum = var1+var2;
+    double diff = var1-var2;
+    double prod = var1*var2;
+    
+    cout<< "var1 =" <<var1<<endl;
+    cout<< "var2 =" <<var2<<endl;
+    cout<< "sum =" <<sum <<endl;
+    cout<< "diff =" <<diff <<endl;
+    cout << "prod= " << prod << endl;*/
+    
+    
+    // q3
+   /* string name;
+    double lab ,midterm, final;
+    cout<< "Enter the student's name: ";
+    cin.ignore();
+    getline(cin, name);
+    
+    cout<< "Enter lab score: ";
+    cin >> lab;
+    cout << "Enter midterm score: ";
+    cin >> midterm;
+    cout<< "Enter final score : ";
+    cin>> final;
+    
+    double last_score = (lab*0.25) + (midterm*0.35 ) + (final*0.4);
+    cout<< "name: " << name << endl;
+    cout<< "lab: " << lab << endl;
+    cout<< "midterm: " << midterm << endl;
+    cout<< "final: " << final << endl;
+    cout<< "last score : " << last_score << endl;*/
+    
+    // q4
+    cout<< "* \n**\n***\n** \n*";
+
+    
+    
+    
+    return 0;
+}
