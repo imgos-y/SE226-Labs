952126fd32ac5aa4a551c1bd8023742e99094f34
imgos-y <imgeyuksel427@gmail.com>
2025-02-20 14:59:47 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+#q1
+studentName = input('what is your name? \n')
+print('Hello' , studentName)
+id = input('What is your student ID? \n ')
+print('your ID is ', id)
+
+#q2
+num1= float(input('Enter the first number: '))
+num2= float(input('Enter the second number: '))
+sum = num1+num2
+diff = num1-num2
+prod = num1* num2
+
+print('summation is: '+str(sum ))
+print('differentation is: ' +str(diff))
+print('multiplication is: ' +str(prod))
+
+#q4
+print("*\n** \n*** \n** \n*")
+
+#q3
+nameOfStudent = input('What is your name? \n')
+labGrade= float(input('enter your lab grade: \n'))
+midtermGrade = float(input('enter your midterm grade: \n'))
+finalGrade = float(input('enter your final grade: \n'))
+lastGrade = labGrade*0.25 + midtermGrade*0.35 + finalGrade*0.4
+print('your last garde is: ' +str(lastGrade))
+
+
+
+
+
+
